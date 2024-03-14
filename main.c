34d140919c71f38d9d4637f61d130c89c189d014
Gonzalosilvalde <72249639+Gonzalosilvalde@users.noreply.github.com>
2024-03-14 15:51:00 +0100

    Entrega

--- main.c
+++ main.c
@@ -1,53 +1,44 @@
 #include "MKL46Z4.h"
 #include "lcd.h"
 
+//constantes
 #define LED_GREEN_PIN 5
 #define LED_RED_PIN 29
 #define SW1_PIN 3
 #define SW2_PIN 12
 
-// Declaración de variables globales
-volatile uint8_t LG = 0; // Variable para el LED verde (0 = apagado, 1 = encendido)
-volatile uint8_t LR = 0; // Variable para el LED rojo (0 = apagado, 1 = encendido)
 
-// Definiciones de macros con cambio de variables globales
-#define LED_GREEN_ON_RED_OFF()   do { LG = 1;LR = 0; GPIOD->PCOR = (1 << LED_GREEN_PIN); GPIOE->PSOR = (1 << LED_RED_PIN);} while(0) // Encender LED verde y establecer LG a 1 apagar rojo y LR 0
-#define LED_GREEN_OFF_RED_ON()  do { LG = 0;LR = 1; GPIOD->PSOR = (1 << LED_GREEN_PIN); GPIOE->PCOR = (1 << LED_RED_PIN);} while(0) // Apagar LED verde y establecer LG a 0 encender rojo y LR 1
-
-
-
-// MACROS estado botones
-#define SW1_PRESSED()    ((GPIOC->PDIR & (1 << SW1_PIN)) == 0) // Verificar si SW1 está presionado
-#define SW2_PRESSED()    ((GPIOC->PDIR & (1 << SW2_PIN)) == 0) // Verificar si SW2 está presionado
-
-//MACRO Funcion interna
-#define IS_HIT() (SW1_PRESSED() && LG) || (SW2_PRESSED() && LR)
-//variables globales
-int i = 0;
-volatile unsigned int sequence = 0x32B14D98;
 
 //structura para guardar los hits y los misses
 typedef struct {
     uint8_t hits;
     uint8_t misses;
 } GameStats;
+// Declaración de variables globales
+volatile uint8_t LedS = 0; // Variable para el LED verde (0 = apagado, 1 = encendido)
+
+uint8_t i = 0;
+volatile unsigned int sequence = 0x32B14D98;
 
 GameStats gameStats = {0};
+
+// Definiciones de macros con cambio de variables globales
+#define LED_GREEN_ON_RED_OFF()  LedS = 1; GPIOD->PCOR = (1 << LED_GREEN_PIN); GPIOE->PSOR = (1 << LED_RED_PIN); // Encender LED verde y establecer LedS a 1 
+#define LED_GREEN_OFF_RED_ON()  LedS = 0; GPIOD->PSOR = (1 << LED_GREEN_PIN); GPIOE->PCOR = (1 << LED_RED_PIN); // Apagar LED verde y establecer LedS a 0 
+
+// MACROS estado botones
+#define SW1_PRESSED()    ((GPIOC->PDIR & (1 << SW1_PIN)) == 0) // Verificar si SW1 está presionado
+#define SW2_PRESSED()    ((GPIOC->PDIR & (1 << SW2_PIN)) == 0) // Verificar si SW2 está presionado
+
+//MACRO Funcion interna para comprobar estado del LedS
+#define IS_HIT() (SW1_PRESSED() && LedS) || (SW2_PRESSED() && !LedS)
 
 //funciones
 void irclk_ini()
 {
-  MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
-  MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
+    MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
+    MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
 }
-/*
-void delay(void)
-{
-  volatile int i;
-
-  for (i = 0; i < 1000000; i++);
-}
-*/
 
 // Led's
 // Funcion para inicializar LEDs modificada para iniciarlos a la vez
@@ -63,7 +54,6 @@ void led_init() {
     GPIOE->PDDR |= (1 << LED_RED_PIN);
 }
 
-
 // Funcion para inicializar los botones
 void sw_init() {
     SIM->COPC = 0;//watchdog
@@ -73,8 +63,8 @@ void sw_init() {
     GPIOC->PDDR |= (1 << SW1_PIN); 
     GPIOC->PSOR = (1 << SW1_PIN);
     PORTC->PCR[SW1_PIN] |= PORT_PCR_IRQC(0xA); // IRQ en el flanco de bajada
-    // Inicializacion del boton sw2
-    
+
+    // Inicializacion del boton sw2 
     PORTC->PCR[SW2_PIN] = PORT_PCR_MUX(1);  
     GPIOC->PDDR |= (1 << SW2_PIN); 
     GPIOC->PSOR = (1 << SW2_PIN);
@@ -84,7 +74,6 @@ void sw_init() {
 
 }
 
-
 void secuencia (void){
     if (sequence & (1 << i)) { // Comprueba el bit en la posición i
         LED_GREEN_ON_RED_OFF();     
@@ -93,12 +82,11 @@ void secuencia (void){
     }
 }
 
-
 void PORTDIntHandler(void) {
 	PORTC->ISFR = 0xFFFFFFFF;	// Clear IRQ
     if (IS_HIT()) {
         ++gameStats.hits;
-    } else if (SW1_PRESSED() || SW2_PRESSED()) {
+    } else {
         ++gameStats.misses;
     }
     secuencia();
@@ -109,9 +97,7 @@ void PORTDIntHandler(void) {
         NVIC_DisableIRQ(31);//descativar interrupciones
     }
     
-    
 }
-
 
 int main(void)
 {
